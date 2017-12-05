8e6897e37639c969f2c53d9f00203a14126a2885
Vaclav Stebra <vstebra@gmail.com>
2017-12-05 19:41:38 +0100

    Day 5 part 2

--- 05_interrupts/index.js
+++ 05_interrupts/index.js
@@ -3,27 +3,25 @@
 const fs = require('fs');
 const assert = require('assert');
 
-const INPUT_01 = `0
+const TEST_INPUT = `0
 3
 0
 1
 -3`;
 
-const INPUT_02 = `0`;
-
 const EXPECTED_OUTPUT_01 = 5;
-const EXPECTED_OUTPUT_02 = 0;
+const EXPECTED_OUTPUT_02 = 10;
 
 const INPUT = fs.readFileSync(__dirname + '/input.txt', 'utf8');
 
-function countSteps(input) {
+function countSteps(input, changeFunction = (x) => x + 1) {
     let interruptTable = input.split('\n').map(Number);
     let index = 0;
     let step = 0;
 
     while (index >= 0 && index < interruptTable.length) {
         const elem = interruptTable[index];
-        interruptTable[index]++;
+        interruptTable[index] = changeFunction(interruptTable[index]);
         index += elem;
         step++;
     }
@@ -31,12 +29,23 @@ function countSteps(input) {
     return step;
 }
 
+function changeFunction02(x) {
+    if (x >= 3) {
+        return x - 1;
+    }
+
+    return x + 1;
+}
+
 function test() {
-    const steps = countSteps(INPUT_01);
+    const steps = countSteps(TEST_INPUT);
     assert.equal(steps, EXPECTED_OUTPUT_01);
+    const steps2 = countSteps(TEST_INPUT, changeFunction02);
+    assert.equal(steps2, EXPECTED_OUTPUT_02);
 }
 
 test();
 
 const result = countSteps(INPUT);
-console.log(result);
+const result02 = countSteps(INPUT, changeFunction02);
+console.log(result, result02);
